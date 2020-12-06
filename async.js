861b52c40779a5e044656a90c0b8f3100b259c8b
rayhomie <1572801584@qq.com>
2020-12-06 20:07:57 +0800

    async

--- async.js
+++ async.js
@@ -23,13 +23,18 @@ console.log('no return:', (async () => {
   await 1
 })().then(d => console.log('no return:', d)))
 
+console.log('return null:', (async () => {
+  return
+})().then(d => console.log('return null:', d)))
 /*
     return promise: Promise { <pending> }
     return await promise: Promise { <pending> }
     return const: Promise { <pending> }
     return await const: Promise { <pending> }
     no return: Promise { <pending> }
+    return null: Promise { <pending> }
     return const: 1
+    return null: undefined
     return await promise: 1
     return await const: 1
     no return: undefined
