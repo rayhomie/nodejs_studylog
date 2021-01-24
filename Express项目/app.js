8c4b632db570f33b4250b18d21cfceb41e8fce75
rayhomie <1572801584@qq.com>
2021-01-24 11:58:01 +0800

    feat: cookie

--- Express项目/app.js
+++ Express项目/app.js
@@ -4,7 +4,7 @@
  * @Author: leihao
  * @Date: 2021-01-21 11:13:17
  * @LastEditors: leihao
- * @LastEditTime: 2021-01-22 14:42:33
+ * @LastEditTime: 2021-01-22 16:54:44
  */
 
 const express = require('express')
@@ -17,7 +17,7 @@ app.use((req, res, next) => {/*匹配任何路由*/
 
   next();/*表示匹配完成这个中间件以后程序继续向下执行*/
 })
-app.use(cookieParser())
+app.use(cookieParser("xiaoduoai"))
 
 //配置静态web目录中间件
 // app.use(express.static('static'))（默认）
@@ -28,16 +28,16 @@ app.set("view engine", "ejs")
 
 app.get('/', (req, res) => {
   res.send(`你好Express
-  <button onclick="handle(1)" >注册</button>
-  <button onclick="handle(2)" >登陆</button>
+  <button onclick="handle(1,window.location.origin)" >注册</button>
+  <button onclick="handle(2,window.location.origin)" >登陆</button>
   <script>
-  function handle(type){
+  function handle(type,host){
     switch(type){
       case 1:
-      window.location.href='http://localhost:3000/register';
+      window.location.href=host+'/register';
       break;
       case 2:
-      window.location.href='http://localhost:3000/login';
+      window.location.href=host+'/login';
     }
   }
   </script>`)
@@ -52,15 +52,18 @@ app.get('/article/add', (req, res, next) => {
 app.get('/article/:id', (req, res) => {
   res.send('动态路由' + req.params.id)//动态路由add
 })
+
 app.get('/product', (req, res) => {
-  res.send('商品页面' + JSON.stringify(req.query))
+  const username = req.signedCookies.username
+  res.send('商品页面' + JSON.stringify(req.query) + username)
 })
 
 app.get('/login', (req, res) => {
   // 设置cookie（maxAge单位ms）
-  res.cookie('username', 'zhangsan', { maxAge: 1000 * 60 * 60 })
+  res.cookie('username', '张三', { maxAge: 1000 * 60 * 60, signed: true })
   res.send('登录页面')
 })
+
 app.get('/register', (req, res) => {
   // 获取cookie
   res.send('注册页面' + (req.cookies.username || ''))
